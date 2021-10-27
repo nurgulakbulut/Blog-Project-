7ee316b8aeb80f54d0385af7f025fe52bbf77aca
nurgulakbulut <nurgulakbulut.95@gmail.com>
2021-10-27 10:39:34 +0000

    Add Queue example

--- app/Mail/NewPostOnFollowedCategory.php
+++ app/Mail/NewPostOnFollowedCategory.php
@@ -4,10 +4,11 @@ namespace App\Mail;
 
 use App\Models\Post;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class NewPostOnFollowedCategory extends Mailable
+class NewPostOnFollowedCategory extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
     public $post;
